05f69fa3893cea61ebaf9840de0b9b9eedc7735a
Jack Kawell <jgkawell@outlook.com>
2019-10-14 21:54:38 -0600

    Added time rounding functionality

--- data_import.py
+++ data_import.py
@@ -1,17 +1,19 @@
+import argparse
 import csv
+import datetime
 import dateutil.parser
 from os import listdir
 from os.path import isfile, join
-import argparse
-import datetime
+from statistics import mean
 
 
 class ImportData:
-    def __init__(self, data_csv):
+    def __init__(self, data_csv, resolution):
+        self.type = self.set_type(data_csv)
         self._time = []
         self._value = []
         self._round_time = []
-        self._round_time_str = []
+        self._round_value = []
 
         time_error = False
         value_error = False
@@ -46,17 +48,64 @@ class ImportData:
                       + "\nOne or more values couldn\'t be parsed."
                       + " Results may be incomplete")
 
+            roundTimeArray(self, resolution)
+
         except FileNotFoundError:
             print(f"File: {data_csv} does not exist.")
 
-    def linear_search_value(self, key_time):
+    def set_type(self, file_name):
+        if "activity" in file_name.lower():
+            return "activity"
+        elif "basal" in file_name.lower():
+            return "basal"
+        elif "bolus" in file_name.lower():
+            return "bolus"
+        elif "cgm" in file_name.lower():
+            return "cgm"
+        elif "hr" in file_name.lower():
+            return "hr"
+        elif "meal" in file_name.lower():
+            return "meal"
+        elif "smbg" in file_name.lower():
+            return "smbg"
+        else:
+            print(f"Unknown type for file: {file_name}")
+
+    def linear_search_value(self, key_time, times, values):
         value_list = []
-        for i in range(len(self._time)):
-            curr = self._time[i]
-            if key_time == curr:
-                value_list.append(self._value[i])
+        for i in range(len(times)):
+            cur_time = times[i]
+            if key_time == cur_time:
+                value_list.append(values[i])
 
         return value_list
+
+
+def roundTimeArray(obj, resolution):
+    unique_time = []
+    for cur_time in obj._time:
+        min_minus = datetime.timedelta(minutes=(cur_time.minute % resolution))
+        min_plus = datetime.timedelta(minutes=resolution) - min_minus
+        if (cur_time.minute % resolution) <= resolution/2:
+            new_time = cur_time - min_minus
+        else:
+            new_time = cur_time + min_plus
+
+        if new_time not in unique_time:
+            unique_time.append(new_time)
+
+        obj._round_time.append(new_time)
+
+    unique_value = []
+    for t in unique_time:
+        values = obj.linear_search_value(t, obj._round_time, obj._value)
+        if obj.type in ["activity", "bolus", "meal"]:
+            unique_value.append(sum(values))
+        elif obj.type in ["smbg", "hr", "cgm", "basal"]:
+            unique_value.append(mean(values))
+
+    obj._round_time = unique_time
+    obj._round_value = unique_value
 
 
 if __name__ == '__main__':
