400c799eafc6db7ea7845f8e5e89b9c59e8e8137
Jack Kawell <jgkawell@outlook.com>
2019-12-05 21:14:25 -0700

    Joined data frams 3)

--- pandas_import.py
+++ pandas_import.py
@@ -25,3 +25,15 @@ if __name__ == '__main__':
 
         # Change value to data name
         df.rename(columns={"value": df_name}, inplace=True)
+
+    # Separte cgm from others
+    df_cgm = None
+    df_list = []
+    for df_name, df in data_dict.items():
+        if df_name != 'cgm':
+            df_list.append(df)
+        else:
+            df_cgm = df
+
+    # Join frames together
+    join_frame = df_cgm.join(df_list, how='left')
