3b8221296ac16c74a1c49e1826b47649ace7f0c9
Jack Kawell <jgkawell@outlook.com>
2019-12-05 20:58:33 -0700

    Formatting according to 2)

--- pandas_import.py
+++ pandas_import.py
@@ -14,3 +14,14 @@ if __name__ == '__main__':
     data_dict['hr'] = pd.read_csv('./smallData/hr_small.csv')
     data_dict['meal'] = pd.read_csv('./smallData/meal_small.csv')
     data_dict['smbg'] = pd.read_csv('./smallData/smbg_small.csv')
+
+    for df_name, df in data_dict.items():
+        # Set the index to time
+        df.index = pd.to_datetime(df.time)
+
+        # Change format to float (drop strings)
+        if df.value.dtype != float:
+            df.apply(lambda x: pd.to_numeric(x, errors='coerce')).dropna()
+
+        # Change value to data name
+        df.rename(columns={"value":df_name},inplace=True)
